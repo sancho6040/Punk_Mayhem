2088a3a34dce974156540b08501ba802487b42d8
sancho6040 <56619610+sancho6040@users.noreply.github.com>
2022-03-12 02:33:53 -0500

    fix_1

--- js/juego.js
+++ js/juego.js
@@ -13,19 +13,27 @@ function preload() {
   // game.load.crossOrigin = 'anonymous';
 
 
-  game.load.tilemap('map', 'assets/tiles/tile_class_EXPORT.csv', null, Phaser.Tilemap.CSV);
-  game.load.spritesheet('player', './assets/Skeleton/SpriteSheets/spritesheet.png', 99.13, 98);
-  game.load.image('platform', './assets/Main/Objects/Obj-Big-Wood-Platform-01.png');
-  game.load.image('granade', './assets/bomb/granade2.png');
-  game.load.spritesheet('kaboom', './assets/bomb/explode.png', 32, 32);64
+  game.load.spritesheet('player', './assets/Chick-Boy/ChikBoy_x32.png', 40, 64);
+  game.load.image('granade', './assets/bomb/granade.png');
+  game.load.spritesheet('boom', './assets/bomb/explode.png', 32, 32);
 
-  game.load.image('tiles', 'assets/tiles/platformer_tiles.png');
+  game.load.image('bg1', './assets/bg/bg.png');
+  game.load.image('bg2', './assets/bg/buildings-bg.png');
+  game.load.image('bg3', './assets/bg/near-buildings-bg.png');
+
+  game.load.tilemap('map', './assets/tileset/tiles_xport.csv', null, Phaser.Tilemap.CSV);
+  game.load.image('tiles', 'assets/tileset_x64.png');
+
 }
 
 var player;
 var platforms;
 var cursors;
 var facingLeft = false;
+
+var bg1;
+var bg2;
+var bg3;
 
 var pig;
 
@@ -40,24 +48,13 @@ var layer;
 
 function create() {
 
-  player = game.add.sprite(300, 4500, 'player');
-  game.camera.follow(player);
-
-  game.physics.arcade.enable(player);
-
-  player.body.collideWorldBounds = true;
-  player.body.gravity.y = 500;
-
-  // player.body.setSize(x,y,offsetX,offsetY);
-
-  // ---------------animation add-------------------
-  player.animations.add('idle', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40], 13, true);
-  player.animations.add('walk', [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57], 13, true);
-  player.animations.add('hit', [16, 17, 18, 19, 20, 21, 22, 23], 13, true);
-  player.animations.add('die', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 13, true);
-
-  
-
+  //-------------------backgounds-------------------------------------
+  bg = game.add.tileSprite(0, 0, 640, 720, 'bg1');
+  bg2 = game.add.tileSprite(0, 0, 640, 720, 'bg2');
+  bg3 = game.add.tileSprite(0, 0, 640, 720, 'bg3');
+  bg.fixedToCamera = true;
+  bg2.fixedToCamera = true;
+  bg3.fixedToCamera = true;
 
   // platforms = game.add.physicsGroup();
 
@@ -68,21 +65,21 @@ function create() {
   // platforms.setAll('body.immovable', true);
 
   //-------------------tiledmap-------------------------
-  map = game.add.tilemap('map', 50, 50);
+  map = game.add.tilemap('map', 32, 32);
   map.addTilesetImage('tiles');
 
   layer = map.createLayer(0);
   layer.resizeWorld();
 
   //map.setCollisionByExclusion([13, 14, 15, 16, 46, 47, 48, 49, 50, 151]);
-  map.setCollisionBetween(0, 107);
+  map.setCollisionBetween(0, 4);
   // map.createFromTiles(0, -1, "coin");
 
   //------------------------------granades---------------------------
   granades = game.add.group();
   granades.enableBody = true;
   granades.physicsBodyType = Phaser.Physics.ARCADE;
-  
+
 
   for (let i = 0; i < 20; i++) {
     var g = granades.create(0, 0, 'granade');
@@ -100,21 +97,40 @@ function create() {
   pigs.physicsBodyType = Phaser.Physics.ARCADE;
 
   pigs.create(450, 400, 'pig');
-   //------------------------input------------------------------------
 
+  //------------------------player---------------------------------------
+  player = game.add.sprite(60, 2430, 'player');
+  game.camera.follow(player);
+
+  game.physics.arcade.enable(player);
+
+  player.body.collideWorldBounds = true;
+  player.body.gravity.y = 500;
+
+  // player.body.setSize(x,y,offsetX,offsetY);
+
+  // ---------------animation add-------------------
+  player.animations.add('idle_left', [20, 21, 22, 23, 24, 25], 6, true);
+  player.animations.add('idle_rigth', [26, 27, 28, 29, 30, 31], 6, true);
+  player.animations.add('run_left', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, true);
+  player.animations.add('run_rigth', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 10, true);
+  player.animations.add('prejump_rigth', [32], 1, true);
+  player.animations.add('prejump_left', [33], 1, true);
+
+  //------------------------input------------------------------------
   cursors = game.input.keyboard.createCursorKeys();
   granadeButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 }
 
 function update() {
+  // console.log(player.x, player.y);
 
-  
   // game.physics.arcade.collide(player, platforms);
   game.physics.arcade.collide(player, layer);
-  
+
   //collision granadas
-  game.physics.arcade.overlap(granades, pigs, collisionHandler, null, this); 
+  game.physics.arcade.overlap(granades, pigs, collisionHandler, null, this);
 
   //-------------------player movement-------------------------------
   player.body.velocity.x = 0;
@@ -122,21 +138,23 @@ function update() {
   if (cursors.left.isDown) {
     //hacia la izquiera
     player.body.velocity.x = -250;
-    player.animations.play('walk');
-    player.scale.x = -1;
+    player.animations.play('run_left');
 
     facingLeft = true;
   }
   else if (cursors.right.isDown) {
     //hacia la derecha
     player.body.velocity.x = 250;
-    player.animations.play('walk');
-    player.scale.x = 1;
+    player.animations.play('run_rigth');
 
     facingLeft = false;
   }
   else {
-    player.animations.play('idle');
+    if (facingLeft) {
+      player.animations.play('idle_left');
+    } else {
+      player.animations.play('idle_rigth');
+    }
   }
 
   if (cursors.up.isDown && (player.body.onFloor() || player.body.touching.down)) {
@@ -161,7 +179,7 @@ function throwGranade() {
   if (!facingLeft) {
     if (game.time.now > granadeTime) {
       granade = granades.getFirstExists(false);
-  
+
       if (granade) {
         granade.reset(player.x, player.y + 8);
         granade.body.velocity.x = 200;
@@ -170,11 +188,11 @@ function throwGranade() {
         granadeTime = game.time.now + 200;
       }
     }
-  }else{
+  } else {
     if (game.time.now > granadeTime) {
       if (game.time.now > granadeTime) {
         granade = granades.getFirstExists(false);
-    
+
         if (granade) {
           granade.reset(player.x, player.y + 8);
           granade.body.velocity.x = -200;
@@ -194,7 +212,7 @@ function resetGranade(granade) {
 }
 
 function collisionHandler(bullet, enemy) {
-  
+
   bullet.kill();
   enemy.kill();
   // box.kill();
